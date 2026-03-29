1f2dfcb4be155f6360b221cfc3ac8daec0cb11f2
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-03-29 12:59:22 +0300

    save function homework1

--- js/test.js
+++ js/test.js
@@ -34,10 +34,40 @@ function onMakeOrderError(error) {
 
 
 
+////////////////////////////////////////////////////////////
 
 
 
 
+
+
+
+
+
+
+
+// Напиши функцію delay(ms), яка повертає проміс, що переходить в стан "resolved" через ms мілісекунд. Значенням промісу, яке виповнилося має бути та кількість мілісекунд, яку передали під час виклику функції delay.
+
+
+
+const delay = ms => {
+  const promise = new Promise((resolve) => {
+    
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+    
+  });
+
+  return promise;
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(logger); 
+delay(1000).then(logger); 
+delay(3000).then(logger);
+delay(1500).then(logger);
 
 
 
